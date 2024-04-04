377fd260a1e551e5d6aac871a42c68f183c6af17
mariapilan <dudapilan06@hotmail.com>
2024-04-04 15:50:46 -0300

    atu

--- src/screens/CadastroProduto.tsx
+++ src/screens/CadastroProduto.tsx
@@ -1,17 +1,38 @@
 import React, { useState } from "react";
 import { Image, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
-import {launchCamera} from "react-native-image-picker";
+import { launchCamera, launchImageLibrary } from "react-native-image-picker";
+import axios from "axios";
 
 const CadastroProduto: React.FC = () => {
-//    const [produtos, setProdutos] = useState<Produto[]>([]);
+    //    const [produtos, setProdutos] = useState<Produto[]>([]);
     const [nome, setNome] = useState<string>('');
     const [preco, setPreco] = useState<string>('');
     const [ingredientes, setIngredientes] = useState<string>('');
     const [imagem, setImagem] = useState<any>('');
-    
-    const CadastroProduto = async()=>{
 
+    const cadastrarProduto = async () => {
+        try {
+            const formData = new FormData();
+            formData.append('nome', nome);
+            formData.append('preco', preco);
+            formData.append('ingredientes', ingredientes);
+            formData.append('imagem', {
+                uri: imagem,
+                type: 'image/jpeg',
+                name: new Date() + '.jpg'
+            });
+
+            const response = await axios.post('http://10.137.11.201:8000/api/produtos', formData, {
+            headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            console.log(response.data)
+        } catch (error) {
+            console.group(error);
+        }
     }
+
     const abrirCamera = () => {
         const options = {
             mediaType: 'photo',
@@ -20,9 +41,9 @@ const CadastroProduto: React.FC = () => {
             maxWidht: 2000
         };
         launchCamera(options, response => {
-            if (response.didCancel){
-            console.log('cancelado pelo usuario')
-            } else if(response.error){
+            if (response.didCancel) {
+                console.log('cancelado pelo usuario')
+            } else if (response.error) {
                 console.log('erro ao abrir a camera')
             } else {
                 let imageUri = response.uri || response.assets?.[0]?.uri;
@@ -32,23 +53,42 @@ const CadastroProduto: React.FC = () => {
         })
     }
 
+    const selecionarImagem = () => {
+        const options = {
+            mediaType: 'photo',
+            includeBase64: false,
+            maxHeight: 2000,
+            maxWidth: 2000
+        };
+
+        launchImageLibrary(options, (response) => {
+            if (response.didCancel) {
+                console.log('cancelado pelo usuario');
+            } else if (response.error) {
+                console.log('erro ao abrir a galeria');
+            } else {
+                let imageUri = response.uri || response.assets?.[0]?.uri;
+            }
+        })
+    }
+
     return (
         <View style={styles.container}>
-            <StatusBar backgroundColor="red" barStyle="light-content"/>
+            <StatusBar backgroundColor="red" barStyle="light-content" />
             <View style={styles.header}>
                 <Text style={styles.headerText}>Top Food</Text>
             </View>
 
             <View style={styles.form}>
-                <TextInput style={styles.input} placeholder="Nome do produto" value={nome} onChangeText={setNome}/>
-                <TextInput style={styles.input} placeholder="Preço" value={preco} onChangeText={setPreco}/>
-                <TextInput style={styles.input} placeholder="Ingredientes" value={ingredientes} onChangeText={setIngredientes} multiline/>
+                <TextInput style={styles.input} placeholder="Nome do produto" value={nome} onChangeText={setNome} />
+                <TextInput style={styles.input} placeholder="Preço" value={preco} onChangeText={setPreco} />
+                <TextInput style={styles.input} placeholder="Ingredientes" value={ingredientes} onChangeText={setIngredientes} multiline />
 
                 <View style={styles.alinhamentoImagemSelecionada}>
-                    {imagem ? <Image source={{uri:imagem}} style={styles.imagemSelecionada}/> : null}
+                    {imagem ? <Image source={{ uri: imagem }} style={styles.imagemSelecionada} /> : null}
                 </View>
 
-                <TouchableOpacity style={styles.imageButton}>
+                <TouchableOpacity style={styles.imageButton} onPress={selecionarImagem}>
                     <Text style={styles.imageButtonText}>Selecionar Imagem</Text>
                 </TouchableOpacity>
 
@@ -56,7 +96,7 @@ const CadastroProduto: React.FC = () => {
                     <Text style={styles.imageButtonText}>Tirar Foto</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity style={styles.button} onPress={cadastrarProduto}>
                     <Text style={styles.buttonText}>Cadastrar Produto</Text>
                 </TouchableOpacity>
 
@@ -66,9 +106,9 @@ const CadastroProduto: React.FC = () => {
     );
 }
 
-const styles=StyleSheet.create({
-    container:{
-        flex:1
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
     },
     header: {
         backgroundColor: 'red',
@@ -83,11 +123,11 @@ const styles=StyleSheet.create({
     form: {
         padding: 10,
         backgroundColor: '#f0f0f0',
-        marginBottom:10
+        marginBottom: 10
     },
     input: {
         height: 40,
-        borderColor:'gray',
+        borderColor: 'gray',
         borderWidth: 1,
         marginBottom: 10,
         paddingHorizontal: 10,
